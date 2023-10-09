6c836185aedbd4c7d0c95f4ffc213097328a83b2
Halsey Filbin <halsey.t.filbin@gmail.com>
2023-10-09 17:27:03 -0500

    Python and JS solved. need help with js testing

--- js/creditCheck.js
+++ js/creditCheck.js
@@ -1,2 +1,43 @@
-exports.creditCheck = function(str) {
-}
+// exports.creditCheck = function(str) {
+// }
+
+// Take in str and split into individual numbers
+// reverse the string
+// double every other number
+// if the doubled number is over 10,split and add them together
+// add all digits
+//if total moddulo 10 equals 0 then number is valid
+
+const creditCheck = function (str) {
+    const digits = str
+        .split("")
+        .reverse()
+        .map(Number)
+        .map((elem, ind) => {
+            if (ind % 2 != 0) {
+                return elem * 2;
+            } else {
+                return elem;
+            }
+        })
+        .map((elem) => {
+            if (elem >= 10) {
+                return elem
+                    .toString()
+                    .split("")
+                    .map(Number)
+                    .reduce((a, b) => a + b);
+            } else {
+                return elem;
+            }
+        })
+        .reduce((a, b) => a + b);
+    if (digits % 10 == 0) {
+        console.log("The number is valid!");
+        return true;
+    } else {
+        console.log("The number is invalid!");
+        return false;
+    }
+};
+
--- python/credit_check.py
+++ python/credit_check.py
@@ -1,5 +1,17 @@
-def credit_check(str):
-    pass
+def credit_check(lst):
+    credit_num=[*lst]
+    for n in range(len(credit_num)):
+        if (n+1)%2 != 0:
+            credit_num[n]=int(credit_num[n])*2
+            if credit_num[n] > 9:
+                credit_num[n]=[*str(credit_num[n])]
+                credit_num[n]=int(credit_num[n][0])+int(credit_num[n][1])
+        else:
+            credit_num[n]= int(credit_num[n])
+    credit_num= sum(credit_num)
+    if credit_num % 10 == 0:
+        return "The number is valid!"
+    return "The number is invalid!"
 
 # Your Luhn Algorithm Here
 # Expected Output:
